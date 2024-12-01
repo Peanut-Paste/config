6270c60188e4d1d3193e9d99245980b0c577e697
Isaac Tan <isaac.tan.2@foodpanda.com>
2024-12-01 16:57:16 +0800

    added macOS install shell

---
+++ macOS/install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# ensure the script is executable "chmod +x install.sh"
+# ensure brew i
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# [$? -ne 0]; then
+	echo "Homebrew installation failed. Exiting."
+	exit 1
+fi
+
+# Install ITerm2
+brew install --cask iterm2
+
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# Download theme
+mkdir downloads
+curl https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/CyberpunkScarletProtocol.itermcolors -- output ./downloads/CyberpunkScarletProtocol.itermcolors
+
+# Install ZSH Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Replace the newly generated .zshrc and use it
+cp -f ./configs/.zshrc ~/.zshrc
+source ~/.zshrc
+
+# Install MySQL
+brew install mysql
+
+# Download sequel pro
+curl -fsSL https://github.com/sequelpro/sequelpro/releases/download/release-1.1.2/sequel-pro-1.1.2.dmg --output ./downloads
+
+# Install vscode
+brew install --cask visual-studio-code
+
+# Installing docker colima kubectl
+brew install docker
+brew install docker-compose
+brew install colima
+brew install kubectl
+
+# Manual work
+echo "\033[0;32m Importing theme into iTerm2"
+echo "\033[0m Manually import CyberpunkScarletProtocol.itermcolors theme into iTerm2"
+echo  "\033[0;32m Install SequelPro"
+echo "\033[0m Check if sequal pro version is latest, install it in the downloads folder"
+echo  "\033[0;32m Setting up VSCode and installing all extensions"
+echo "\033[0m set shell command: 'install ... then run\n cat ./configs/vscode/extensions.list |% { code --install-extension \$_}"
+echo  "\033[0;32m Create new ssh keys for github, etc"
+echo "\033[0m ssh-keygen -t ed25519 -C 'your_email@example.com'"
