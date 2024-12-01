58076a86a8217e7bf981bdb66dc3b6c1ac899fda
peanut-paste <isaacjingyi1997@gmail.com>
2024-12-01 20:23:57 +0800

    removed test.sh, and added to install docker

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -32,6 +32,20 @@ xargs -n 1 code --install-extension < ./configs/vscode/extensions.list
 sudo curl -fsSL  https://go.dev/dl/go1.23.3.linux-amd64.tar.gz --output ./downloads/go1.23.3.linux-amd64.tar.gz
 sudo tar -C $HOME -xzf ./downloads/go1.23.3.linux-amd64.tar.gz
 
+# installing docker and docker-compose
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+
 # Manual work
 echo  "\033[0;32m Fixing powerlevel10k theme font"
 echo "\033[0m Install all 4 fonts in the download/fonts folder."
--- ubuntu/test.sh
+++
@@ -1,4 +0,0 @@
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-echo $DIR
-sudo curl -fsSL  https://go.dev/dl/go1.23.3.linux-amd64.tar.gz --output ./downloads/go1.23.3.linux-amd64.tar.gz
-sudo tar -C $HOME -xzf ./downloads/go1.23.3.linux-amd64.tar.gz
